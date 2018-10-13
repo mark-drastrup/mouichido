fd63c4591bbfa636ab13981902ea83973ec0414b
mark_drastrup <mark.drastrup@gmail.com>
2018-10-13 19:36:10 +0200

    Adding database configuration and server file

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+const epilogue = require('epilogue');
+const OktaJwtVerifier = require('@okta/jwt-verifier');
+const User = require("../db/models").User;
+const Entry = require("../db/models").Entry;
+
+
+app.listen(3000, () => {
+    console.log("App is running on port 3000")
+})
